50512e1e69f5a3dc44da20b328ce2a31262c37de
alfrsanc <alfrsanc@student.42madrid.com>
2025-10-01 19:33:05 +0200

    complete function ft_isalpha

--- libtf/ft_isalpha.c
+++ libtf/ft_isalpha.c
@@ -9,10 +9,10 @@
 /*   Updated: 2025/09/29 18:20:24 by alfrsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int (int c)
+#include <unistd.h>
+int isalpha(int c)
 {
-    if (i < 64 && i > 123)
+    if ((c > 64 && c < 91) || (c > 96 && c < 123))
     {
         return (1);
     }
@@ -21,3 +21,27 @@ int (int c)
         return (0);
     }
 }
+
+/*int	main(int c, char *v[])
+{
+	char		i;
+    int result;
+
+
+    if (c == 2)
+	{
+		i = 0;
+        result = isalpha(v[1][0]);
+        if(result == 1) {
+            write(1, "es una letra", 13);
+        } else {
+            write(1, "no es una letra", 16);
+        }
+    }
+    else
+    {
+        write(1, "No es un caracter", 17);
+    }
+
+	return (1);
+}*/
