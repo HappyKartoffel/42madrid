fdd26814da53379841fa40abd3bbb6d9abeab2d7
alfrsanc <alfrsanc@student.42madrid.com>
2025-10-08 14:01:20 +0200

    update of libtf.h

--- libtf/libtf.h
+++ libtf/libtf.h
@@ -6,7 +6,7 @@
 /*   By: alfrsanc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 18:20:21 by alfrsanc          #+#    #+#             */
-/*   Updated: 2025/10/08 13:27:59 by alfrsanc         ###   ########.fr       */
+/*   Updated: 2025/10/08 14:00:54 by alfrsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ char			*ft_strrchr(const char *str, int c);
 int				ft_strncmp(char *s1, char *s2, unsigned int len);
 int				ft_tolower(char *str);
 int				ft_toupper(char *str);
-
+void			ft_calloc(unsigned int nmemb, unsigned int size);
+char			*ft_strdup(const char *s);
 #endif
