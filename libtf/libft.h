b00e9dde0940ca08a2084e5b423e48755c06d0d6
alfrsanc <alfrsanc@student.42madrid.com>
2025-10-17 18:52:42 +0200

    pasa norminette

--- libtf/libft.h
+++ libtf/libft.h
@@ -6,12 +6,12 @@
 /*   By: alfrsanc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 18:14:31 by alfrsanc          #+#    #+#             */
-/*   Updated: 2025/10/17 18:29:53 by alfrsanc         ###   ########.fr       */
+/*   Updated: 2025/10/17 18:48:55 by alfrsanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBTF_H
-# define LIBTF_H
+#ifndef LIBFT_H
+# define LIBFT_H
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
